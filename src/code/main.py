9f73e08105f779e71b63782695617c73f3387e55
Vilho Knuutila <knuutila.vilho@gmail.com>
2021-12-22 00:51:48 +0200

    added logging to depression detection and changed cooldown length

--- src/code/main.py
+++ src/code/main.py
@@ -24,7 +24,7 @@ import random
 
 #SETUP
 
-cooldowns = {"train":60, "water":30, "find":60, "shop":25, "feed":15, "depression":43200}
+cooldowns = {"train":60, "water":30, "find":60, "shop":25, "feed":15, "depression":86400}
 
 custom_cooldowns = {}
 
@@ -239,6 +239,7 @@ async def on_message(message):
 		result = await update_cooldowns(message.author, "depression")
 		if result:
 			return
+		print(f"Depression detected on {message.guild}, message content was '{message.content}'")
 		viesti = discord.Embed(title="Depression Help", description = f"Hey, I saw you were talking about sad things on {message.guild} and wanted to say that there are people that love and care about you! Hope you are okay.\n\nIf you want information about helplines for depressed and suicidal people use command `/ineedhelp`", color = c)
 		try:
 			channel = await message.author.create_dm()
